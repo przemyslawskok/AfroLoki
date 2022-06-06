eadc93357031ad20384b0284f00f659482de34c5
przemyslawskok <przemyslaw.jump@gmail.com>
2022-06-06 22:22:53 +0200

    zmiana nazw podstron, panel do edycji trzech cen, krotkie wyswietlanie na stronie glownej - dodany na sztywno cennik

--- app.py
+++ app.py
@@ -67,10 +67,16 @@ class posty(db.Model):
    opis = db.Column(db.String(500))
 
 
-def __init__(self, zdjecia, opis):
-   self.zdjecia = zdjecia
-   self.opis = opis
+   def __init__(self, zdjecia, opis):
+      self.zdjecia = zdjecia
+      self.opis = opis
+class ceny(db.Model):
+   cena_id = db.Column('cena_id', db.Integer, primary_key = True)
+   cena = db.Column(db.Integer())
 
+
+   def __init__(self,cena):
+      self.cena = cena
 
 db.create_all()
 
@@ -94,19 +100,24 @@ def glowna():
       slownik[rekord.post_id]=[tablica,rekord.opis]
          
 
-
+   rekordy=ceny.query.order_by(ceny.cena_id).all()
+   lista=[]
+   for rekord in rekordy:
+         lista.append([rekord.cena_id,rekord.cena])
+      
 
 
 
    return render_template("/glowna.html",
    slownik=slownik,
+   lista=lista,
    liczba_postow=str(len(rekordy)))
 
 
-@app.route('/dodaj_post')
-def dodaj_post():
+@app.route('/panel')
+def panel():
    if uzytkownik_zalogowany():
-   
+      
       rekordy=posty.query.order_by(posty.post_id.desc()).all()
       slownik={}
     
@@ -124,22 +135,57 @@ def dodaj_post():
          slownik[rekord.post_id]=[tablica,rekord.opis]
          
 
+      rekordy=ceny.query.order_by(ceny.cena_id.desc()).all()
+      lista=[]
+    
+      for rekord in rekordy:
+         lista.append([rekord.cena_id,rekord.cena])
+
+        
+         
+  
 
 
 
 
 
-      return render_template("/dodaj_post.html",
+
+      return render_template("/panel.html",
       slownik=slownik,
+      lista=lista,
       wejscia=ilosc_wejsc.licznik,
       liczba_postow=str(len(rekordy)))
    else:
-      return redirect("/dodaj_post_logowanie")
+      return redirect("/panel_logowanie")
 
 
 
+
+@app.route('/zaktualizuj_ceny',methods=["POST"])
+def zaktualizuj_ceny():
+   if uzytkownik_zalogowany():
+      cena_1=request.form.get("cena_1")
+      cena_2=request.form.get("cena_2")
+      cena_3=request.form.get("cena_3")
+
+      rekordy=ceny.query.order_by(ceny.cena_id.desc()).all()
+      print(rekordy)
+      for rekord in rekordy:
+         if rekord.cena_id==3:
+            rekord.cena=int(cena_3)
+            db.session.commit()
+         if rekord.cena_id==2:
+            rekord.cena=int(cena_2)
+            db.session.commit()
+         if rekord.cena_id==1:
+            rekord.cena=int(cena_1)
+            db.session.commit()
+
+
+
+      return redirect("/panel")
 @app.route('/dodaj_post',methods=["POST"])
-def dodaj_post_post():
+def dodaj_post():
    if uzytkownik_zalogowany():
 
       posortowane=posty.query.order_by(posty.post_id.desc()).all()
@@ -178,10 +224,10 @@ def dodaj_post_post():
       db.session.commit()
 
 
-      return redirect("/dodaj_post")
+      return redirect("/panel")
       
    else:
-      return redirect("/dodaj_post_logowanie")
+      return redirect("/panel_logowanie")
 
 @app.route('/usun_post',methods=["POST"])
 def usun_post_post():
@@ -215,17 +261,17 @@ def usun_post_post():
 
 
 
-@app.route('/dodaj_post_logowanie')
-def dodaj_post_logowanie():
+@app.route('/panel_logowanie')
+def panel_logowanie():
    if uzytkownik_zalogowany():
-      return redirect("/dodaj_post")
+      return redirect("/panel")
    else:
       if logowanie.bledne:
          logowanie.bledne=False
-         return render_template("dodaj_post_zle.html")
+         return render_template("panel_zle.html")
       else:
 
-         return render_template("dodaj_post_logowanie.html")
+         return render_template("panel_logowanie.html")
 
 
 
@@ -238,10 +284,10 @@ def zaloguj_sie_postowink():
    login=str(login)
    if password=="haslo" and login=="loki":
       session['sjnsda8i3j2k4a,mSDAlk*@nb2']="87ashknm2afm,cxvnxc"
-      return redirect("/dodaj_post")
+      return redirect("/panel")
    else:
       logowanie.bledne=True
-      return redirect('/dodaj_post_logowanie')
+      return redirect('/panel_logowanie')
    
 
 
